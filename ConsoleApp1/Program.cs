bb041d11f92c492621f828957582b7fe7e45112e
Junior gonçalves <decimo.terceiro13@hotmail.com>
2022-10-11 21:40:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Console.WriteLine("Hello, World!");
+//Console.Write("Congratulations!");
+//Console.Write("");
+//Console.Write("You wrote your first lines of code!");
+
+//a linguagem considera os espaço, e WriteLine pula linha 
+
+
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
+
+
+//declarando variveis 
+
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
